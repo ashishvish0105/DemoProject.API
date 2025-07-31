a2212040b346dca1606d23887c2952b76e6b5046
Ashish Premchand Vishwakarma <ashishvish0105@gmail.com>
2025-07-31 10:00:19 +0530

    add login controller and remove unnecessary file

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public string Login(string name,string password)
+        {
+            return "login";
+        }
+    }
+}
